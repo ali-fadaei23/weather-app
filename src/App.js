c18b328588dfcdc022c5ebd2e0e94bbfa1fab7a5
Ali Fadaei <razorn1375@gmail.com>
2020-07-31 22:51:48 +0430

    Create component weather data

--- src/App.js
+++ src/App.js
@@ -3,26 +3,64 @@ import React, { Component } from "react";
 
 // Components
 import Weather from "./Components/Weather";
-
+import WeatherData from "./Components/WeatherData";
 import "./App.css";
 
 const API_KEY = "d14f4fe2ef55d830d97042742e784457";
 
 class App extends Component {
+  state = {
+    temperature: null,
+    city: null,
+    country: null,
+    description: null,
+    humidity: null,
+    speed: null,
+    error: null,
+  };
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.city.value;
-    const country = e.target.country.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
     const data = await api_call.json();
     console.log(data);
+    if (city) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        speed: data.wind.speed,
+        error: " ",
+      });
+    } else {
+      this.setState({
+        temperature: null,
+        city: null,
+        country: null,
+        description: null,
+        humidity: null,
+        speed: null,
+        error: "please Enter The City..",
+      });
+    }
   };
   render() {
     return (
       <div>
         <Weather getWeather={this.getWeather} />
+        <WeatherData
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          speed={this.state.speed}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,23 +1,22 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 
 const Weather = (props) => (
   <Form onSubmit={props.getWeather}>
+    <DropdownButton name="city" title="Dropdown button">
+      <Dropdown.Item>Tehran</Dropdown.Item>
+      <Dropdown.Item>Shiraz</Dropdown.Item>
+      <Dropdown.Item>Ahvaz</Dropdown.Item>
+    </DropdownButton>
     <Form.Control
       type="text"
       placeholder="City...."
       name="city"
       style={{ width: "150px" }}
     />
-    
-    <Form.Control
-      type="text"
-      placeholder="Country...."
-      name="country"
-      style={{ width: "150px" }}
-    />
-
     <Button variant="primary" type="submit">
       Get Weather
     </Button>
