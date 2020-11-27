f5bb4556ecc451ba19d65c0be5cb77755d89790a
Ali Fadaei <razorn1375@gmail.com>
2020-11-27 12:40:08 +0330

    Create CSS of components

--- src/Components/FormWeather.js
+++ src/Components/FormWeather.js
@@ -1,25 +1,23 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 
 const FormWeather = (props) => (
-  <Form onSubmit={props.onSubmit}>
-    <Card style={{ width: "50%", alignItems: "center", margin: " 15px auto " }}>
-      <Card.Body style={{ margin: "5px auto" }}>
-        <div>
-          <Form.Control
-            type="text"
-            placeholder="City...."
-            name="city"
-            style={{ width: "150px" }}
-          />
-          <Button variant="primary" type="submit" style={{ margin: "5px" }}>
-            Get Weather
-          </Button>
-        </div>
-      </Card.Body>
-    </Card>
+  <Form
+    onSubmit={props.onSubmit}
+    style={{ width: "50%", alignItems: "center", margin: " 15px auto " }}
+  >
+    <div style={{ margin: "5px auto" }}>
+      <Form.Control
+        type="text"
+        placeholder="City...."
+        name="city"
+        style={{ width: "150px" }}
+      />
+      <Button variant="primary" type="submit" style={{ margin: "5px" }}>
+        Get Weather
+      </Button>
+    </div>
   </Form>
 );
 
